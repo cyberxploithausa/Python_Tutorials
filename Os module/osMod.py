fbe199f5eb772f75de9aae78275fd1d3a64be87e
Cyberxploit <58391231+cyberxploit@users.noreply.github.com>
2021-07-13 14:52:43 +0100

    First commit

---
+++ Os module/osMod.py
@@ -0,0 +1,24 @@
+import os
+
+print(dir(os))
+
+
+# current_dir = os.getcwd()
+# print(current_dir)
+
+# list_dir = os.listdir()
+# print(list_dir)
+
+# new_dir = os.mkdir('main')
+# print(new_dir)
+
+# # os.makedirs('main2/sub_dir')
+# os.chdir('C:/Users/cyberxploit/Desktop/Youtube/Os module/main2/')
+# print(os.getcwd())
+
+# os.removedirs('main2/sub_dir')
+
+
+
+
+
